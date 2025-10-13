70720b326432039ac7471f558e0bc6efc06fe49e
jahnavipurimitla6-glitch <jahnavipurimitla6@gmail.com>
2025-10-13 09:56:25 +0530

    Add files via upload

---
+++ janu68.py
@@ -0,0 +1,21 @@
+#Mutli Label Inheritance
+class A:
+
+      def displayA(self):
+          print("Inheritance A Class")
+
+
+ class B(A):
+       def displayB(self):
+           print("Inheritance B Class")
+
+ class C(B):
+       def displayC(self):
+            print("Inheritance C Class")
+
+
+obj = C()
+obj.displayA()
+obj.displayB()
+obj.displayC()
+            
