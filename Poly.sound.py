d006e65a514d76d67c76f542885c4ab7c3bd74be
jahnavipurimitla6-glitch <jahnavipurimitla6@gmail.com>
2025-10-17 10:58:48 +0530

    Add files via upload

---
+++ Poly.sound.py
@@ -0,0 +1,13 @@
+class Dog:
+
+   def sound(self):
+        print("Woof")
+        
+class Cat:
+   def sond(self):
+         print("Meow")
+
+animals=[Dog(),Cat()]
+
+for animals in animals:
+    animal.sound()
